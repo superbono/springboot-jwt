d6c22e87f4176cb42896b1af8a560d415766423a
superbono <choiar@pincar.co.kr>
2022-01-17 16:40:22 +0900

    JWT 토큰 생성 및 테스트

--- src/main/java/com/example/jwt/config/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/example/jwt/config/jwt/JwtAuthorizationFilter.java
@@ -1,5 +1,7 @@
 package com.example.jwt.config.jwt;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.example.jwt.config.auth.PrincipalDetails;
 import com.example.jwt.model.User;
 import com.example.jwt.repository.UserRepository;
@@ -11,9 +13,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 
 // 스프링 시큐리티에서 UsernamePasswordAuthenticationFilter가 있다.
@@ -47,8 +52,6 @@ public class JwtAuthorizationFilter extends UsernamePasswordAuthenticationFilter
         System.out.println("JwtAuthorizationFilter : 진입");
         // username, password를 입력후에
         // 로그인 시도해야함.
-
-
         try {
             ObjectMapper om = new ObjectMapper();
             User user = om.readValue(request.getInputStream(), User.class);
@@ -56,18 +59,53 @@ public class JwtAuthorizationFilter extends UsernamePasswordAuthenticationFilter
 
             UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword());
 
+            // DB에 있는 username과 password가 일치한다.
             Authentication authentication = authenticationManager.authenticate(authenticationToken);
 
             PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
-            System.out.println("유저정보"+principalDetails.getUser().getUsername());
+            System.out.println("로그인완료 -> username:  "+principalDetails.getUser().getUsername());
             System.out.println("=====================1");
             // authentication 객체가 session영역에 저장됨.
+            // authentication 객체를 리턴하는 이유는 권한 관리를 security가 대신해주기 때문에 authentication을 리턴해준다.
+            // JWT 토큰을 사용하면 따로 security session영역에 저장하지 않아도 된다. 그래도 리턴을 하는 이유는 권한 처리를 하기 위함이다.
+
             return authentication;
         } catch (IOException e) {
             e.printStackTrace();
         }
+
             System.out.println("=====================2");
             return null;
+    }
 
+    // attemptAuthentication에서 인증이 완료되고 종료되면 successfulAuthentication 함수가 실행된다.
+    // successfulAuthentication함수 안에서 jwt 토큰을 만든다.
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+        System.out.println("successfulAuthentication: 인증완료");
+        PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
+
+        // RSA방식x , Hash암호방식
+        String jwtToken = JWT.create()
+                             .withSubject("R_token")
+                             .withExpiresAt(new Date(System.currentTimeMillis()+ (60000*10)))
+                             .withClaim("id",principalDetails.getUser().getId())
+                             .withClaim("username",principalDetails.getUser().getUsername())
+                             .sign(Algorithm.HMAC512("R2"));
+        /*
+        * 1.일반적인 방법
+        * 요청할때마다 클라이언트 쿠키에 담은 세션ID를 항상 들고 서버에 요청할 시에 사용됨.
+        * session.getAttribute("세션값 확인")
+        * 서버는 클라이언트 요청이 오면, 세션ID가 유효한지 판단후에 세션ID가 유효할 경우, 인증이 필요한 페이지로 접근한다.
+        *
+        * 2.formLogin방식을 사용하지 않는 경우
+        * username, password의 인증이 완료된 경우,
+        * JWT 토큰을 생성
+        * 클라이언트쪽으로 jwt토큰을 응답해준다 (header의 Authorization속성 안에 넣어준다.)
+        * 요청시에는 세션ID가 아닌, JWT토큰을 가지고 요청해야 한다.
+        * 서버는 JWT토큰이 유효한지를 판단 (필터를 만들어줘야 한다.)
+        */
+        response.addHeader("Authorization","Bearer "+jwtToken);
     }
 }
